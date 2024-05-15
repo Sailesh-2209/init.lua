ab5b5f402f5aec8a5ca26354b6f078d7f84b5822
Sailesh-SparkCognition <srajaputra@sparkcognition.com>
2024-05-15 13:26:29 +0530

    added package for indentation guides

---
+++ after/plugin/ibl.lua
@@ -0,0 +1,7 @@
+-- configuration for the indent-blank-line package
+-- source - https://github.com/lukas-reineke/indent-blankline.nvim
+
+ibl = require("ibl")
+
+ibl.setup()
+
--- lua/sailesh/lazy.lua
+++ lua/sailesh/lazy.lua
@@ -15,24 +15,25 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 plugins = {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	{ "nvim-treesitter/nvim-treesitter", name = "treesitter", build = ":TSUpdate" },
-	{ "nvim-lua/plenary.nvim" },
-	{ "nvim-telescope/telescope-fzy-native.nvim" },
-	{ "nvim-telescope/telescope.nvim", tag = "0.1.5" },
-	{
-		"jiaoshijie/undotree",
-		dependencies = "nvim-lua/plenary.nvim",
-		config = true,
-		keys = { 
-			{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
-		}
-	},
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'L3MON4D3/LuaSnip'},
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "nvim-treesitter/nvim-treesitter", name = "treesitter", build = ":TSUpdate" },
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope-fzy-native.nvim" },
+    { "nvim-telescope/telescope.nvim", tag = "0.1.5" },
+    {
+        "jiaoshijie/undotree",
+        dependencies = "nvim-lua/plenary.nvim",
+        config = true,
+        keys = { 
+            { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+        }
+    },
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
 }
 opts = {}
 
